2b4864a7c84d0664267a3c34977410a60f010ac9
CKStew <75023746+CKStew@users.noreply.github.com>
2020-12-20 03:11:02 +0900

    minigame 버그 수정 및 변경
    1. UI를 출력할 때 깜빡거리지 않게 변경.
    2. UI를 출력할 때 커서가 중구난방으로 나오는 문제 수정.
    3. 틀린 답을 입력했을 때 이제 "Wrong Answer"를 출력하고 다음 문제로 넘어감.
    4. 이제 단어가 내려오는 속도의 최소값이 50으로 고정됨.
    5. 사용하지 않는 함수들 삭제.

--- Project1/Project1/minigame.h
+++ Project1/Project1/minigame.h
@@ -12,7 +12,7 @@ private:
 	int row;		//ui의 row 개수(세로 길이)
 	int column;		//ui의 column 개수(가로 길이)
 	int score;		//맞춘 단어 수
-	int speed;		//문자가 내려가는 속도
+	double speed;		//문자가 내려가는 속도
 
 
 
@@ -21,12 +21,10 @@ private:
 public:
 	MiniGame();
 	~MiniGame();
-	void get_input();	//getch()로 input을 받음. 이걸 사용하면 문자가 계속 내려가지 않고 멈추기 때문에 사용하지 않음.
 	void print_input();	//str을 출력함.
 	void ui_clear(int clear_row);	//ui를 clear_row까지 공백(' ')으로 바꿔서 지움.
 	void ui_print();	//ui를 출력함.
-	void make_start();	//원래는 단어 동전쌓기처럼 만드려고 한 건데 바뀜. 사용하지 않음. 
 	void game(string word);	//word를 전달받아 산성비처럼 내려가게 하고, input을 입력받아 맞는 경우 score를 +1함.
-	void coin_stacking();
-	int return_score();	//score를 출력함.
+	void print_score();		//score를 앞 뒤 줄바꿈과 함께 출력함.
+	int return_score();	//score를 return함.
 };
